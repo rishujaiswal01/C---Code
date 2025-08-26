bcdc224d4edaa5765a5c58d9d556340174c3bbae
Rishu Jaiswal <rishujaiswal8560@gmail.com>
2025-08-26 10:20:06 +0530

    All Files Saved

---
+++ sub.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a = 15 , b = 8 ;
+    int sub = a - b ;
+    printf("Sub of a and b is %d",sub); 
+    return  0 ;
+}
---
+++ sum.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a = 5 , b = 8 ;
+    int sum = a + b ;
+    printf("Sum of a and b is %d",sum); 
+    return  0 ;
+}

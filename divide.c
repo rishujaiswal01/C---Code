46e31bf6569be4869370b0e1e350fef84a035abe
Rishu Jaiswal <rishujaiswal8560@gmail.com>
2025-08-26 10:36:13 +0530

    All Files Saved

---
+++ divide.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a = 14 , b = 2 ;
+    int divide  = a/b ;
+    printf("divide  of a and b is %d",divide); 
+    return  0 ;
+}
---
+++ multi.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a = 5 , b = 8 ;
+    int mult = a * b ;
+    printf("mult of a and b is %d",mult); 
+    return  0 ;
+}
